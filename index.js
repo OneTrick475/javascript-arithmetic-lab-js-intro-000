535b280459fd4826b5dffb3a1f1f31c7a6bb7a7e
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-04-29 13:48:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ function add(a, b){
  return  a + b
 }
 
-function add(a, b){
+function substract(a, b){
  return  a - b
 }
 
-function add(a, b){
+function multiply(a, b){
  return  a * b
 }
 
-function add(a, b){
+function divide(a, b){
  return  a / b
 }
